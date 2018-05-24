0061db57a66bc2f186a121655e04fe76c83473d3
CUzer <cengizuzer@hotmail.com>
2018-05-24 02:57:48 -0700

    Split example

--- src/main/java/com/cuzer/springbatch/configuration/JobConfiguration.java
+++ src/main/java/com/cuzer/springbatch/configuration/JobConfiguration.java
@@ -1,6 +1,5 @@
 package com.cuzer.springbatch.configuration;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,8 +9,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;;
 
 @Configuration
 @EnableBatchProcessing
@@ -23,7 +21,6 @@ public class JobConfiguration {
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
 
-	
 	public Step step1() {
 		return stepBuilderFactory.get("step1").tasklet(new Tasklet() {
 
@@ -36,7 +33,6 @@ public class JobConfiguration {
 		}).build();
 	}
 
-	
 	public Step step2() {
 		return stepBuilderFactory.get("step2").tasklet((contribution, chuckContext) -> {
 			System.out.println(">> Step 2 executed.");
@@ -55,27 +51,24 @@ public class JobConfiguration {
 		}).build();
 	}
 
-//	@Bean
-//	public Job hellowWorldJob() {
-//		return jobBuilderFactory.get("helloWorldJob")
-//				.start(step1())
-//				.next(step2())
-//				.next(step3())
-//				.build();
-//	}
+	// @Bean
+	// public Job hellowWorldJob() {
+	// return jobBuilderFactory.get("helloWorldJob")
+	// .start(step1())
+	// .next(step2())
+	// .next(step3())
+	// .build();
+	// }
 
-	
-	
-	@Bean
-	public Job TransitionJob() {
-		return jobBuilderFactory.get("transitionJob")
-				.start(step1())
-				.on("COMPLETED").to(step2())
-//				.from(step2()).on("COMPLETED").to(step3())
-				.from(step2()).on("COMPLETED").stopAndRestart(step3())
-				
-				
-				.from(step3()).end()
-				.build();
-	}
+	// @Bean
+	// public Job TransitionJob() {
+	// return jobBuilderFactory.get("splitJob")
+	// .start(step1())
+	// .on("COMPLETED").to(step2())
+	//// .from(step2()).on("COMPLETED").to(step3())
+	// .from(step2()).on("COMPLETED").stopAndRestart(step3())
+	// .from(step3()).end()
+	// .build();
+	// }
+
 }
